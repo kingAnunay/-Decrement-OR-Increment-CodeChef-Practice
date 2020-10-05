5f9b8f1806054f835aec6d6542bf59ae47e909d4
KingAnunay <anunaynmit@gmail.com>
2020-10-05 20:39:48 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n = 0;
+    cin>>n;
+    if(n>=0&&n<=1000){
+        if(n%4==0){
+            n++;
+            
+        }
+        else{
+            n--;
+        }
+        std::cout<<n;
+    }
+	return 0;
+}
